2e63f6efe53cf2e8f09e77520bba443d1946b9bd
Gugino <gugino.joshua@gmail.com>
2026-08-06 20:44:07 -0400

    Added init of main command, options added, svelte or sveltekit selection added.

--- src/index.ts
+++ src/index.ts
@@ -1,6 +1,6 @@
 #!/usr/bin/env node
 
-import { Command } from "commander";
+import { Command, Option } from "commander";
 import * as prompt from "@clack/prompts";
 
 const program = new Command();
@@ -11,10 +11,26 @@ program
     "A CLI tool for creating SvelteKit projects with Pocketbase intergrations such as auth.",
   )
   .argument("[project-name]", "name of the project")
-  .action(async (projName) => {
-    prompt.intro("svelbase")
-    const name = projName ?? await prompt.text({ message: "Project name?" })
+  .addOption(new Option("--ts, --typescript", "Use Typescript").default(true).conflicts(["--js", "--javascript"]))
+  .addOption(new Option("--js, --javascript", "Use JavaScript").conflicts(["--ts", "--typescript"]))
+  .addOption(new Option("--tw, --tailwind", "Use Tailwind"))
+  .addOption(new Option("--docker", "Include a docker-compose for PocketBase"))
+  .action(async (projectName: string | undefined, opts) => {
+    prompt.intro(`svelbase (Typescript)`)
+    const name = projectName ?? await prompt.text({ message: "Project name?" })
+
+    const svelteType = await prompt.select({
+      message: "Svelte or SvelteKit?",
+      options: [
+        {label: "Svelte (Vite)", value: "svelte"},
+        {label: "SvelteKit", value: "sveltekit"}
+      ]
+    })
+
+    prompt.note(`Name: ${String(name)} | Type: ${String(svelteType)}`)
+    //Create project
+
     prompt.outro("Project setup complete!")
   });
 
-program.parse()
+program.parseAsync()
